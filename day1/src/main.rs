7b133e80e97d9a37ccb96abbe09016ad0f9179a3
timotree3 <timorcb@gmail.com>
2020-12-12 13:41:07 -0500

    Day 1 part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,36 @@
+use snafu::{ResultExt, Snafu};
+use std::collections::HashSet;
+use std::io::BufRead;
+
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu(display("Could not read next line from stdin: {}", source))]
+    ReadLine { source: std::io::Error },
+    #[snafu(display("Could not parse line as integer {}: {}", line, source))]
+    ParseLine {
+        line: String,
+        source: std::num::ParseIntError,
+    },
+    #[snafu(display("Could not find a valid pair of integers"))]
+    NotFound,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = std::io::stdin();
+    let mut input = stdin.lock();
+    println!("{}", run(&mut input).unwrap());
+}
+
+fn run<R: BufRead>(input: R) -> Result<u32, Error> {
+    let mut seen = HashSet::new();
+    for line in input.lines() {
+        let line = line.context(ReadLine)?;
+        let int: u32 = line.parse().context(ParseLine { line })?;
+        let complement = 2020 - int;
+        if seen.contains(&complement) {
+            return Ok(int * complement);
+        }
+        seen.insert(int);
+    }
+    Err(Error::NotFound)
 }

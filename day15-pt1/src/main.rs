5a9a76b2c770824cb424dc080f2957e97422ac75
timotree3 <timorcb@gmail.com>
2020-12-15 00:49:58 -0500

    Day 15 pt 1

---
+++ day15-pt1/src/main.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut ages: HashMap<usize, usize> = [14, 8, 16, 0, 1, 17]
+        .iter()
+        .enumerate()
+        .map(|(era, &num)| (num, era))
+        .collect();
+    let mut difference = 0;
+    let mut t = ages.len();
+    let mut last_spoken = difference;
+    while t < 2019 {
+        // dbg!(&ages, last_spoken, difference);
+        let new = difference;
+        difference = t - ages.get(&new).unwrap_or(&t);
+        ages.insert(new, t);
+        t += 1;
+        last_spoken = difference;
+    }
+    println!("answer: {}", last_spoken);
+}
